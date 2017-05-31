e00f7714b9627adbdaa88f1c36ef28231c9282ae
hamny <hm0089@naver.com>
2017-05-31 14:14:03 +0900

    2017-5-31 혜민 트럭 목록 기능 추가중1

--- Project_dreamtruck/src/main/java/com/kosta/abbo/user/controller/TruckUserController.java
+++ Project_dreamtruck/src/main/java/com/kosta/abbo/user/controller/TruckUserController.java
@@ -4,17 +4,26 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kosta.abbo.HomeController;
 import com.kosta.abbo.user.service.TruckUserService;
 
-@Service
-@RequestMapping("/truckUser")
+@Controller
+@RequestMapping("/truck")
 public class TruckUserController {
 	@Inject
 	private TruckUserService service;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@RequestMapping(value="/list", method = RequestMethod.GET)
+	public void list(Model model) throws Exception {
+		logger.info(".....show truck list ");
+		model.addAttribute("list", service.list());
+	}
 }
